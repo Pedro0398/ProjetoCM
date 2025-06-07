51015c0cba94f7524ca4bff2fae759dc3b25c431
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2025-06-07 16:04:51 +0100

    feat: upadte

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.flutter_application_1"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.13113456"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
